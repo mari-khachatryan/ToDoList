a7611fb7416cc6a0eef094e9e752fb777bf1a621
mari-khachatryan <104691300+mari-khachatryan@users.noreply.github.com>
2023-06-14 17:33:35 +0400

    Update toDoList.cpp

--- toDoListCpp/toDoList.cpp
+++ toDoListCpp/toDoList.cpp
@@ -19,68 +19,65 @@ void ToDoList::updateTask(int number, const std::string& desc, bool comp, Date d
     if(number < 0 || number > tasks.size()) {
         throw std::out_of_range("number is out of range");
     }
-    Task t = tasks[number];
+    Task& t = tasks[number];
     t.setCompleted(comp);
     t.setDescription(desc);
     t.setDate(d);
 }
 
-void ToDoList::updateTask(int number, const std::string& desc) {
+void ToDoList::updateTaskDesc(int number, const std::string& desc) {
     if(number < 0 || number > tasks.size()) {
         throw std::out_of_range("number is out of range");
     }
-    Task t = tasks[number];
+    Task& t = tasks[number];
     t.setDescription(desc);
-}
+   }
 
-void ToDoList::updateTask(int number, bool comp) {
+void ToDoList::updateTaskComp(int number, bool comp) {
     if(number < 0 || number > tasks.size()) {
         throw std::out_of_range("number is out of range");
     }
-    Task t = tasks[number];
+    Task& t = tasks[number];
     t.setCompleted(comp);
 }
 
-void ToDoList::updateTask(int number, Date d) {
+void ToDoList::updateTaskDate(int number, Date d) {
     if(number < 0 || number > tasks.size()) {
         throw std::out_of_range("number is out of range");
     }
-    Task t = tasks[number];
+    Task& t = tasks[number];
     t.setDate(d);
 }
 
 void ToDoList::viewTasks() {
-    std::cout << "Hello" ;
     Date date;
     time_t now = time(0);
     tm* ltm = localtime(&now);
     date.setYear(1900 + ltm->tm_year);
     date.setMonth(1 + ltm->tm_mon);
-    date.setDay(ltm->tm_yday);
+    date.setDay(ltm->tm_mday);
     std::string res;
+    std::cout << date.getDay()<< "-" << date.getMonth()<<"-"<<date.getYear();
     for(int i{0}; i < tasks.size(); ++i) {
         if(tasks[i].getCompleted()) {
             res = "Completed";
-            system("Color 0A");
             std::cout << i + 1 << ". " << tasks[i].getDescription() << " | " << res 
             << " | " << tasks[i].getDate().getDateStr()  << std::endl;
         } else {
             res = "not completed";
-            if(date.getDay() - tasks[i].getDate().getDay() == 1) {
+            if(tasks[i].getDate().getDay() - date.getDay() == 1) {
                 std::cout << i + 1 << ". " << tasks[i].getDescription() << " | " << res 
-                << " | "  << tasks[i].getDate().getDateStr() << std::endl;
-                system("Color E4");
-                std::cout << "deadline ends in 1 day!!";
-            }else if(date.getDay() - tasks[i].getDate().getDay() < 0) {
+                << " | "  << tasks[i].getDate().getDateStr() << " | ";
+                std::cout << "deadline ends in 1 day!!" << std::endl;
+            }else if(tasks[i].getDate().getDay() - date.getDay() < 0) {
                 std::cout << i + 1 << ". " << tasks[i].getDescription() << " | " << res 
-                << " | " << tasks[i].getDate().getDateStr() << std::endl;
-                system("Color E4");
-                std::cout << "Deadlines are missed!!";
+                << " | " << tasks[i].getDate().getDateStr() << " | ";
+                std::cout << "Deadlines are missed!!" << std::endl;
             } else {
                 std::cout << i + 1 << ". " << tasks[i].getDescription() << " | " << res
-                << " | " << tasks[i].getCompleted() << std::endl;
+                << " | " << tasks[i].getDate().getDateStr() << std::endl;
             }
-        }
+        } 
     }
 }
 
