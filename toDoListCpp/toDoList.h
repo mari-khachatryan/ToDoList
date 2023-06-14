5399e1232d82b7f7f5e12ab1889f8b4b569b9cc9
mari-khachatryan <104691300+mari-khachatryan@users.noreply.github.com>
2023-06-14 17:34:10 +0400

    Update toDoList.h

--- toDoListCpp/toDoList.h
+++ toDoListCpp/toDoList.h
@@ -13,14 +13,16 @@ public:
     void addTask(const Task& t);
     void delateTask(int number);
     void updateTask(int number, const std::string& desc, bool comp, Date d);
-    void updateTask(int number, const std::string& desc);
-    void updateTask(int number, bool comp);
-    void updateTask(int number, Date d);
+    void updateTaskDesc(int number, const std::string& desc);
+    void updateTaskComp(int number, bool comp);
+    void updateTaskDate(int number, Date d);
     void viewTasks();
     int getCountOfComp();
     int getCountOfNotComp();
 private:
     std::vector<Task> tasks;
 };
+
+#endif  //TODOLIST_H
 
 #endif  //TODOLIST_H
